74cf5cfd80d2a18cef959b071aa9f6b70ddee209
Iryna Bilyk <imelnyk@rightandabove.com>
2023-05-10 16:08:48 +0300

    added moca lecture

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,78 @@
+describe("Positive test suit", () => {
+  it("Test 1", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 2", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 3", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 4", () => {
+    cy.visit('https://example.com');
+  })
+})
+
+
+
+
+describe("Negative test suit", () => {
+  it("Test 1", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 2", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 3", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 4", () => {
+    cy.visit('https://example.com');
+  })
+}) 
+
+
+
+
+describe("Some test suit", () => {
+
+  it("Test 1", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 2", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 3", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 4", () => {
+    cy.visit('https://example.com');
+  })
+
+  describe("Some another test suit", () => {
+    it("Test 1", () => {
+      cy.visit('https://example.com');
+    })
+    
+    it("Test 2", () => {
+      cy.visit('https://example.com');
+    })
+    
+    it("Test 3", () => {
+      cy.visit('https://example.com');
+    })
+    
+    it("Test 4", () => {
+      cy.visit('https://example.com');
+    })
+  }) 
+}) 
