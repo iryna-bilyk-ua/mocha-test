c94294d617d163766e0bf7a82473f025c7a4b3b9
Iryna Bilyk <imelnyk@rightandabove.com>
2023-05-10 16:14:48 +0300

    added feature/homework

---
+++ cypress/e2e/before-after.cy.js
@@ -0,0 +1,11 @@
+describe("Positive test suit", () => {
+
+  it("Test 1", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 2", () => {
+    cy.visit('https://example.com');
+  })
+
+})
